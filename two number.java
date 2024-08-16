d9b47fe7f135ca679721834873c2e1af9b2d4caa
Vivek7709 <vivekchaudhari9765@gmail.com>
2024-08-16 12:30:15 +0530

    Add files via upload

---
+++ two number.java
@@ -0,0 +1,13 @@
+class main{
+    public ststic void main(string[]args){
+
+        int first = 10;
+
+        int second = 20;
+
+        // add two numbers
+        int sum = first + second ;
+        system.out.println(first + "+"+ second +"="+ sum);
+        
+    }
+}
